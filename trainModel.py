bbe58f7469c5616f9b4b5b14d749e3c51c0354f0
Simon Swan <shks2@cam.ac.uk>
2018-03-20 14:53:17 +0000

    Increased validation logging frequency.

--- trainModel.py
+++ trainModel.py
@@ -97,24 +97,25 @@ epoch=1
 #TC.validation_data=(vX,vY)
 logOUT = []
 for i in range(10):
-    vloss,vacc,batch2tot = 0,0,0
-    for batch2 in test:
-        #print(batch2)
-        [vx,vy] = batch2
-        [val_loss,val_acc] = model.test_on_batch(vx,vy)
-        vloss += val_loss
-        vacc += val_acc
-        batch2tot += 1
-    vloss = vloss/batch2tot
-    vacc = vacc/batch2tot
     batchNO=0
     for batch in train:
         [x,y] = batch
         [loss, acc] = model.train_on_batch(x,y)
         batchNO += 1
-        logOUT.append([epoch,batchNO,loss,acc,vloss,vacc])
         if batchNO%200 == 0:
+            vloss,vacc,batch2tot = 0,0,0
+            for batch2 in test:
+                #print(batch2)
+                [vx,vy] = batch2
+                [val_loss,val_acc] = model.test_on_batch(vx,vy)
+                vloss += val_loss
+                vacc += val_acc
+                batch2tot += 1
+            vloss = vloss/batch2tot
+            vacc = vacc/batch2tot
             print('Batch number: %d.%d Validation accuracy: %f Validation loss: %f'%(epoch, batchNO, vacc, vloss))
+
+        logOUT.append([epoch,batchNO,loss,acc,vloss,vacc])
     epoch += 1
 
 print('Saving training logs...')

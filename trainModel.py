c9de77b4cc0d7b8d0aa062080d9379afc0857c81
Simon Swan <shks2@cam.ac.uk>
2018-04-09 11:50:11 +0100

    added model import for modularity of train_model.py

--- trainModel.py
+++ trainModel.py
@@ -38,58 +38,11 @@ rd.shuffle(train)
 
 print('Batches generated: %d training batches, %d validation batches.'%(len(train),len(test)))
 
+exec(compile(open('./models/lstm_double.py')))
 
-batch_size = 10
-
-model = Sequential()
-model.add(word_vectors.get_keras_embedding(train_embeddings=True))
-model.add(LSTM(30,
-               activation='tanh', # activation function used
-               recurrent_activation='hard_sigmoid', # activation function for recurrent step
-               use_bias=True, # whether the layer uses a bias vector
-               kernel_initializer='glorot_uniform', # initialiser for the weights matrix
-               recurrent_initializer='orthogonal', # initialiser for the recurrent kernal's weights
-               bias_initializer='zeros', # initialiser for the bias vector
-               unit_forget_bias=True, # add 1 to the bias of the forget gate at initialization
-               kernel_regularizer=None, # regularizer function applied to kernal
-               recurrent_regularizer=None, # regularizer function applied to recurrent kernal
-               bias_regularizer=None, # regularizer function applied to bias vector
-               activity_regularizer=None, # regularizer function applied to output of the layer
-               kernel_constraint=None, # constraint function applied to the kernal
-               recurrent_constraint=None, # constraint function applied to the recurrent kernal
-               bias_constraint=None, # constraint function applied to the bias vector
-               dropout=0.0, # fraction of units to drop for the linear transformation of the inputs
-               recurrent_dropout=0.0, # fraction of units to drop for the linear transformation of the recurrent state
-               implementation=1, # implementation mode, either 1 or 2.
-               return_sequences=False,
-               return_state=False,
-               go_backwards=False,
-               stateful=False, # If True, the last state for each sample at index i in a batch will be used as initial state for the sample of index i in the following batch.
-               unroll=False)) # whether the network will be unrolled, otherwise a symbolic loop will be used.
-model.add(Dense(2,
-                activation='tanh',
-                use_bias=True,
-                kernel_initializer='glorot_uniform',
-                bias_initializer='zeros',
-                kernel_regularizer=None,
-                bias_regularizer=None,
-                activity_regularizer=None,
-                kernel_constraint=None,
-                bias_constraint=None))
-model.compile(optimizer='RMSprop',
-              loss='binary_crossentropy',
-              metrics=['acc'],
-              sample_weight_mode=None,
-              weighted_metrics=None,
-              target_tensors=None)
 #model.load_weights('./logs/wordvec_model.h5',by_name=True)
 model.summary()
 #plot_model(model, to_file='./images/modalityLSTMmodel.png', show_shapes=True)
-
-#TC = TensorBoard(log_dir='./logs/LSTM_Neg_Spec', batch_size=batch_size,
-#                          histogram_freq=0, write_images=True,
-#                          write_grads=False, write_graph=True, embeddings_freq=1)
-
 model_checkpoint = ModelCheckpoint('./logs/LSTMmodel.h5')
 
 epoch=1
@@ -122,6 +75,10 @@ print('Saving training logs...')
 with open('./logs/LSTMmodelLOG.csv', 'w') as logFile:
     writer = csv.writer(logFile)
     writer.writerows(logOUT)
+
+print('Generating Confusion Matrix')
+
+
 print('Saving model weights...')
 model.save_weights('./logs/LSTMmodel.h5')
 

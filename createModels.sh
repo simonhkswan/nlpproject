fef3d53e5fe699c9326d29798cca2efc7fb0ae18
simonhkswan <shks2@cam.ac.uk>
2018-05-01 16:50:20 +0100

    updated create models shell script.

--- createModels.sh
+++ createModels.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-python trainmodel.py /models/lstm_double.py /embeddings/wordvec.bin /logs/wordvec1/
+models=("/models/lstm_double.py")
+
+for $i in "${models[@]}"; do
+echo 'Model: ' $i
+python trainmodel.py -model_location $i -embedding_location /embeddings/wordvec.bin -logs_dest /logs/wordvec1/
+done
+wait

afc582a0e44da313a388fb4f9c0157a10380a74d
simonhkswan <shks2@cam.ac.uk>
2018-05-07 20:16:43 +0100

    training for single lstm fixed embedding.

--- createModels.sh
+++ createModels.sh
@@ -4,4 +4,5 @@
 
 #python trainModel.py models/lstm_double.py logs/lstm1win2/ embeddings/PubMed-shuffle-win-2.bin
 
-python trainModel.py models/lstm_single.py logs/lstm2iter17/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin
+python trainModel.py models/lstm_single_embedFixed.py logs/lstm2iter17/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin
+python evaluateModel.py models/lstm_single.py logs/lstm2iter17/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin

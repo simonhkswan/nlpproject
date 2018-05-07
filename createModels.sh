9df486dcd04617165c8812f5e5e651bea425659c
simonhkswan <shks2@cam.ac.uk>
2018-05-07 17:09:14 +0100

    updated to train single lstm.

--- createModels.sh
+++ createModels.sh
@@ -2,6 +2,6 @@
 
 #python trainModel.py models/lstm_double.py logs/lstm1iter17/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin
 
-python trainModel.py models/lstm_double.py logs/lstm1win2/ embeddings/PubMed-shuffle-win-2.bin
+#python trainModel.py models/lstm_double.py logs/lstm1win2/ embeddings/PubMed-shuffle-win-2.bin
 
-python trainModel.py models/lstm_double.py logs/lstm1win30/ embeddings/PubMed-shuffle-win-30.bin
+python trainModel.py models/lstm_single.py logs/lstm2iter17/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin

44ddcea1b4544a624933337c302bad92722cd6f9
simonhkswan <shks2@cam.ac.uk>
2018-05-07 12:29:43 +0100

    added print debug statment.

--- nlpcore.py
+++ nlpcore.py
@@ -298,6 +298,7 @@ def generate_batches(sentences, maxlen, batchsize, embed_dict):
 
 
 def conf_matrix(y_true, y_pred, title='Confusion Matrix', threshold=50, filename=None):
+    print(y_true,y_pred)
     activation = np.linspace(-1,1,50)
     f_max = 0
     fs = []

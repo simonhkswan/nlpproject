f9ecf9fadfb0b98e9f0eee8838e2904b6c05e1fa
simonhkswan <shks2@cam.ac.uk>
2018-05-07 16:39:11 +0100

    updated shell script to try the large embeddings.

--- createModels.sh
+++ createModels.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python trainModel.py models/lstm_double.py logs/lstm1iter17/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin
+#python trainModel.py models/lstm_double.py logs/lstm1iter17/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin
+
+python trainModel.py models/lstm_double.py logs/lstm1win2/ embeddings/PubMed-shuffle-win-2.bin
+
+python trainModel.py models/lstm_double.py logs/lstm1win30/ embeddings/PubMed-shuffle-win-30.bin

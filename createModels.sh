d663a7876055fb4a69ac0564469a0d8f72a26e8a
simonhkswan <shks2@cam.ac.uk>
2018-05-09 15:38:59 +0100

    added new values of hidden layers to test.

--- createModels.sh
+++ createModels.sh
@@ -9,11 +9,11 @@
 
 #python trainModel.py models/lstm_single_embedDual.py logs/lstm2iter17_dual/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin
 #python evaluateModel.py models/lstm_single_embedDual.py logs/lstm2iter17_dual/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin
-hidden=("10" "20" "30" "40" "50" "60" "70" "80")
+hidden=("10" "80" "90" "100" "150" "200")
 
 for v in "${hidden[@]}"
 do
   echo models/lstm_single.py logs/lstm2iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
-  #python trainModel.py models/lstm_single.py logs/lstm2iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
+  python trainModel.py models/lstm_single.py logs/lstm2iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
   python evaluateModel.py models/lstm_single.py logs/lstm2iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
 done

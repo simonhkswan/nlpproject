d985f30d9f57bac6c3e934710d15874308d7a339
simonhkswan <shks2@cam.ac.uk>
2018-05-10 21:40:35 +0100

    added training for larger embeddings

--- createModels.sh
+++ createModels.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-#python trainModel.py models/lstm_double.py logs/lstm1win2/ embeddings/PubMed-shuffle-win-2.bin
+python trainModel.py models/lstm_double.py logs/lstm1win2_hid30/ embeddings/PubMed-shuffle-win-2.bin --value 30
+python evaluateModel.py models/lstm_double.py logs/lstm1win2_hid30/ embeddings/PubMed-shuffle-win-2.bin --value 30
+
+python trainModel.py models/lstm_double.py logs/lstm1win30_hid30/ embeddings/PubMed-shuffle-win-30.bin --value 30
+python evaluateModel.py models/lstm_double.py logs/lstm1win30_hid30/ embeddings/PubMed-shuffle-win-30.bin --value 30
 
 #python trainModel.py models/lstm_single_embedFixed.py logs/lstm2iter17_fixed/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin
 #python evaluateModel.py models/lstm_single.py logs/lstm2iter17_fixed/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin
@@ -8,7 +12,7 @@
 #python trainModel.py models/lstm_single_embedDual.py logs/lstm2iter17_dual/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin
 #python evaluateModel.py models/lstm_single_embedDual.py logs/lstm2iter17_dual/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin
 
-hidden=("40" "50" "100" "150" "200" "500")
+#hidden=("40" "50" "100" "150" "200" "500")
 
 #for v in "${hidden[@]}"
 #do
@@ -17,9 +21,9 @@ hidden=("40" "50" "100" "150" "200" "500")
 #  python evaluateModel.py models/lstm_single.py logs/lstm2iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
 #done
 
-for v in "${hidden[@]}"
-do
-  echo models/lstm_double.py logs/lstm1iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
-  python trainModel.py models/lstm_double.py logs/lstm1iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
-  python evaluateModel.py models/lstm_double.py logs/lstm1iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
-done
+#for v in "${hidden[@]}"
+#do
+#  echo models/lstm_double.py logs/lstm1iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
+#  python trainModel.py models/lstm_double.py logs/lstm1iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
+#  python evaluateModel.py models/lstm_double.py logs/lstm1iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
+#done

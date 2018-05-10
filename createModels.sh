d1c1a1f58ac7faeb4d6214c7437411646350b00c
simonhkswan <shks2@cam.ac.uk>
2018-05-10 16:08:19 +0100

    shell script updated for lstm double

--- createModels.sh
+++ createModels.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#python trainModel.py models/lstm_double.py logs/lstm1iter17/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin
-
 #python trainModel.py models/lstm_double.py logs/lstm1win2/ embeddings/PubMed-shuffle-win-2.bin
 
 #python trainModel.py models/lstm_single_embedFixed.py logs/lstm2iter17_fixed/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin
@@ -12,9 +10,16 @@
 
 hidden=("10" "20" "30" "40" "50" "100" "150" "200" "500")
 
+#for v in "${hidden[@]}"
+#do
+#  echo models/lstm_single.py logs/lstm2iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
+#  python trainModel.py models/lstm_single.py logs/lstm2iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
+#  python evaluateModel.py models/lstm_single.py logs/lstm2iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
+#done
+
 for v in "${hidden[@]}"
 do
-  echo models/lstm_single.py logs/lstm2iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
-  python trainModel.py models/lstm_single.py logs/lstm2iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
-  python evaluateModel.py models/lstm_single.py logs/lstm2iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
+  echo models/lstm_double.py logs/lstm1iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
+  python trainModel.py models/lstm_double.py logs/lstm1iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
+  python evaluateModel.py models/lstm_double.py logs/lstm1iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
 done

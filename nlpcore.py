corrected f-score calculation in confusion matrix.

nlpcore.py
@@ -307,8 +307,8 @@ def conf_matrix(y_true, y_pred, title='Confusion Matrix', threshold=50, filename
         y_act = np.where(y_pred>a, 1, 0)
         cm = confusion_matrix(y_true,y_act)
         cm2 = normalize(cm,axis=1,norm='l1')
-        p = float(cm2[0][0])
-        r = float(cm2[1][1])
+        p = float(cm2[1][1])
+        r = float(cm[1][1]/(cm[1][1]+cm[1][0]))
         try:
             f = 2*p*r/(p+r)
         except ZeroDivisionError:
7427e703c7aec076c5dfe2d29a5c803128e1f39d
simonhkswan <shks2@cam.ac.uk>
2018-05-10 12:32:44 +0100

    updated list of hidden variables.

--- createModels.sh
+++ createModels.sh
@@ -10,7 +10,7 @@
 #python trainModel.py models/lstm_single_embedDual.py logs/lstm2iter17_dual/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin
 #python evaluateModel.py models/lstm_single_embedDual.py logs/lstm2iter17_dual/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin
 
-hidden=("10" "20" "30")
+hidden=("10" "20" "30" "40" "50" "100" "150" "200" "500")
 
 for v in "${hidden[@]}"
 do

3025a4a6c9cf4197a1ed5ae88035346d0e33b237
simonhkswan <shks2@cam.ac.uk>
2018-05-11 12:06:19 +0100

    updated train model for negation matrix generation.

--- trainModel.py
+++ trainModel.py
@@ -96,7 +96,7 @@ for i in range(10):
             if not os.path.exists(args.logs_dest+'confmatrix/'):
                 os.makedirs(args.logs_dest+'confmatrix/')
             #neg_f, neg_a = conf_matrix(corr_Y[:,0], pred_Y[:,0], filename = args.logs_dest+'confmatrix/neg_epoch%2d.%d.png'%(epoch,batchNO))
-            spec_f, spec_a = conf_matrix(corr_Y[:,1], pred_Y[:,1], filename = args.logs_dest+'confmatrix/spec_epoch%2d.%d.png'%(epoch,batchNO))
+            spec_f, spec_a = conf_matrix(corr_Y[:,0], pred_Y[:,0], filename = args.logs_dest+'confmatrix/neg_epoch%2d.%d.png'%(epoch,batchNO))
 
         [x,y] = batch
         [loss, acc] = model.train_on_batch(x,y)

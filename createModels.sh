0eaf66b8a41496e32b176ef41a668aa332b8de26
simonhkswan <shks2@cam.ac.uk>
2018-05-14 01:26:06 +0100

    updated script for new training

--- createModels.sh
+++ createModels.sh
@@ -12,7 +12,7 @@
 #python trainModel.py models/lstm_single_embedDual.py logs/lstm2iter17_dual/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin
 #python evaluateModel.py models/lstm_single_embedDual.py logs/lstm2iter17_dual/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin
 
-hidden=("30" "40" "50" "100" "150" "200")
+hidden=("20" "300" "400")
 
 #for v in "${hidden[@]}"
 #do
@@ -31,18 +31,18 @@ hidden=("30" "40" "50" "100" "150" "200")
 for v in "${hidden[@]}"
 do
 echo _______________ $v _______________
-python trainModel.py models/bilstm_single.py logs/bilstm2iter17_drop_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
-python evaluateModel.py models/bilstm_single.py logs/bilstm2iter17_drop_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
-python evaluateModelF.py models/bilstm_single.py logs/bilstm2iter17_drop_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
+#python trainModel.py models/bilstm_single.py logs/bilstm2iter17_drop_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
+#python evaluateModel.py models/bilstm_single.py logs/bilstm2iter17_drop_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
+#python evaluateModelF.py models/bilstm_single.py logs/bilstm2iter17_drop_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
 
-#python trainModel.py models/lstm_single.py logs/lstm2iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
-#python evaluateModel.py models/lstm_single.py logs/lstm2iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
-#python evaluateModelF.py models/lstm_single.py logs/lstm2iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
+python trainModel.py models/lstm_single.py logs/lstm2iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
+python evaluateModel.py models/lstm_single.py logs/lstm2iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
+python evaluateModelF.py models/lstm_single.py logs/lstm2iter17_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
 
-#python trainModel.py models/lstm_single_embedDual.py logs/lstm2iter17_dual_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
-#python evaluateModel.py models/lstm_single_embedDual.py logs/lstm2iter17_dual_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
-#python evaluateModelF.py models/lstm_single_embedDual.py logs/lstm2iter17_dual_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
+python trainModel.py models/lstm_single_embedDual.py logs/lstm2iter17_dual_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
+python evaluateModel.py models/lstm_single_embedDual.py logs/lstm2iter17_dual_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
+python evaluateModelF.py models/lstm_single_embedDual.py logs/lstm2iter17_dual_hid$v/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value $v
 
-#python trainModel.py models/lstm_single_embedFixed.py logs/lstm2iter17_fix_hid100/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value 100
-#python evaluateModel.py models/lstm_single_embedFixed.py logs/lstm2iter17_fix_hid100/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value 100
+python trainModel.py models/lstm_single_embedFixed.py logs/lstm2iter17_fix_hid100/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value 100
+python evaluateModel.py models/lstm_single_embedFixed.py logs/lstm2iter17_fix_hid100/ embeddings/low_shuff_combine_tokenized.txt-iter17-min5.bin --value 100
 done
